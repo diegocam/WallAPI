4657390eb5893b34a87f2da8d0132a357afd9732
Diego Camacho <camachojcd@gmail.com>
2019-01-11 06:33:03 -0800

    Create Posts model

---
+++ app/Models/Post.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Carbon\Carbon;
+
+class Post extends Model
+{
+    protected $appends = ['updated_when'];
+    protected $dates = ['created_at', 'updated_at'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function getUpdatedWhenAttribute($value)
+    {
+        return $this->updated_at->diffForHumans();
+    }
+
+}
