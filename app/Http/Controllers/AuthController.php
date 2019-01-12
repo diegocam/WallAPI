224e0827a0a2203f1056609496f8529134722a81
Diego Camacho <camachojcd@gmail.com>
2019-01-12 01:28:39 -0800

    Use Hash for app config consistency

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -21,7 +21,7 @@ class AuthController extends Controller
             'first_name' => $request->input('first_name'),
             'last_name' => $request->input('last_name'),
             'email' => $request->input('email'),
-            'password' => bcrypt($request->input('password')),
+            'password' => Hash::make($request->input('password')),
         ]);
         $user->save();
 
